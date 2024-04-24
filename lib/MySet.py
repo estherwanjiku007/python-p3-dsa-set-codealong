681ac5d56997e1443744789465af4e17c42fe6e4
estherwanjiku007 <estherwanjiqu@gmail.com>
2024-04-24 12:12:34 +0300

    Made the final touches

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,16 +1,35 @@
-class MySet:
-    def __init__(self,enumerable=[]):
-        self.dictionary={}
-        for value in enumerable:
-            self.dictionary[value]=True
+# class MySet:
+#     def __init__(self,enumerable=[]):
+#         self.dictionary={}
+#         for value in enumerable:
+#             self.dictionary[value]=True
 
-    def has(self,value) :
-        return value in self.dictionary    
+#     def has(self,value) :
+#         return value in self.dictionary    
+    
+#     def add(self,value):
+#         self.dictionary[value]=True
+#         return self
+
+#     def delete(self,value):
+#         self.dictionary.pop(value,None)
+#         return self
+    
+#     def size(self):
+#         return len(self.dictionary)
+class MySet:
+    def __init__(self,enumarable=[]):
+        self.dictionary={}
+        for value in enumarable:
+            self.dictionary[value]=True
+    
+    def has(self,value):
+        return value in self.dictionary
     
     def add(self,value):
         self.dictionary[value]=True
         return self
-
+    
     def delete(self,value):
         self.dictionary.pop(value,None)
         return self
@@ -18,6 +37,5 @@ class MySet:
     def size(self):
         return len(self.dictionary)
 
-        
 
     
